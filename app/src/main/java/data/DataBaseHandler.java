c2d798601a4b5d10d9689ed16534c8c4c0001a0e
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-30 07:51:19 +0300

    метод, который будет возвращать список всех объектов в bd ...

--- app/src/main/java/data/DataBaseHandler.java
+++ app/src/main/java/data/DataBaseHandler.java
@@ -1,11 +1,16 @@
 package data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.LinearLayout;
 
-import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Car;
 import utils.Util;
 
 /*
@@ -30,5 +35,82 @@ public class DataBaseHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
         onCreate(db);
+    }
+
+
+    /* -------------------- CRUD --------------------- */
+
+    public void addCar(Car car) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // этот объект нужен, чтобы взаимодействовать с db
+        // что-то вроде HashMap() - ключ-значение
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Util.KEY_NAME, car.getName());
+        contentValues.put(Util.KEY_PRICE, car.getPrice());
+
+        // теперь вставляем нашу запись в db
+        db.insert(Util.TABLE_NAME, null, contentValues);
+        db.close();
+    }
+
+    public Car getCar(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // объект этого класса позволит перемещаться по db
+        Cursor cursor = db.query(
+                Util.TABLE_NAME,
+                new String[] {Util.KEY_ID, Util.TABLE_NAME, Util.KEY_PRICE,},
+                Util.KEY_ID + "=?",
+                new String[] {String.valueOf(id)},
+                null, null, null, null);
+
+        if (cursor != null) {
+            // метод возвращает true, если объект cursor содержит записи
+            cursor.moveToFirst();
+        }
+
+        /*
+            создается объект, который будем возвращать - в него извлекается вся
+            информация из нашего cursor
+         */
+        Car car = new Car(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+
+        return car;
+    }
+
+    /*
+        этот метод будет возвращать список объектов
+        используем List<> потому-что это интерфейс от который имплементируют другие способы
+        записи в список (ArrayList и т.п) - Если мы захоим в будущем использовать любой из них
+        нам не нужно будет переделывать код ... )))
+     */
+    public List<Car> getAllCars() {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        List<Car> carsList = new ArrayList<>();
+
+        /*
+            создаем строку в которую будет помещен SQL код
+            SELECT выбираем * означает все FROM откуда
+         */
+        String selectAllCars = "SELECT * FROM " + Util.TABLE_NAME;
+
+        // теперь нужне объект класса Cursor, который позволит пермещатся по db и выбирать нужные данные
+        Cursor cursor = db.rawQuery(selectAllCars, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Car car = new Car();
+                car.setId(Integer.parseInt(cursor.getString(0)));
+                car.setName(cursor.getString(1));
+                car.setPrice(cursor.getString(2));
+
+                carsList.add(car);
+
+            } while (cursor.moveToNext()); // что значит пока есть следующий элемент в cursor
+        }
+
+        return carsList;
     }
 }
