30e385dc30bd8a6be79ecda748f6f5c68258b6bb
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-30 08:05:43 +0300

    тестируем работу методов addCar() and getAllCars()

--- app/src/main/java/com/omelchenkoaleks/carsdb/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/carsdb/MainActivity.java
@@ -3,6 +3,12 @@ package com.omelchenkoaleks.carsdb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import data.DataBaseHandler;
+import model.Car;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // СОЗДАЕМ ОБЪЕКТ, КОТОРЫЙ УПРАВЛЯЕТ БАЗОЙ ДАННЫХ
+        DataBaseHandler dataBaseHandler = new DataBaseHandler(this);
+
+        // С ПОМОЩЬЮ ЭТОГО ОБЪЕКТА ДОБАВЛЯЕМ В БАЗУ ДАННЫЕ
+        dataBaseHandler.addCar(new Car("Toyota", "30 000 $"));
+        dataBaseHandler.addCar(new Car("Opel", "25 000 $"));
+        dataBaseHandler.addCar(new Car("Mercedes", "50 000 $"));
+        dataBaseHandler.addCar(new Car("KIA", "28 000 $"));
+
+        // СОЗДАЕМ ПУСТОЙ СПИСОК, В КОТОРЫЙ ИЗВЛЕКАЕМ ВСЕ ЗАПИСИ ИЗ db
+        List<Car> carList = dataBaseHandler.getAllCars();
+
+        // ВЫВОДИМ В ЖУРНАЛ ВСЕ ЧТО НАХОДИТСЯ В ЭТОМ ЛИСТЕ
+        for (Car car : carList) {
+            Log.d("Car Info: ", "ID: " + car.getId()
+                    + " NAME: " + car.getName() + " PRICE: " + car.getPrice());
+        }
     }
 }
